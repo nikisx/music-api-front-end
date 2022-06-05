195ef11354cb9c35ea8eaace794c8d0570f14a9b
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-05 23:41:44 +0300

    add search filter for albums and playlist

--- src/components/Albums/Albums.vue
+++ src/components/Albums/Albums.vue
@@ -3,11 +3,36 @@
       <h1>Albums</h1>
       <a v-if="user" @click="showModal(null)" class="create-btn">Create
       Album</a>
+      <article style="margin-top: 50px; margin-left: 174px;">
+      <div style="margin: 0" class="wrap">
+        <div style="left: 37em;" class="search">
+            <input type="text" v-model="nameSearch" class="searchTerm"
+            placeholder="Name" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="number" v-model.number="yearSearch" class="searchTerm"
+            placeholder="Year" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="number" v-model.number="durationSearch" class="searchTerm"
+            placeholder="Duration" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+    </div>
+   </article>
       <div v-if="!albums.length">
       No Albums :( please add from <a @click="showModal(null)" style="color: #42b983; cursor: pointer;">here</a>
     </div>
       <section class="albums-wrapper">
-        <div @click="openAlbumModal(album)" v-for="(album, index) in albums" :key="index" class="polaroid">
+        <div @click="openAlbumModal(album)" v-for="(album, index) in filteredAlbums" :key="index" class="polaroid">
           <img :src="album.imageUrl" style="width:100%">
           <div class="container">
               <img v-if="user && album.createdById == user.id" @click.stop="showModal(album)"
@@ -48,6 +73,9 @@ export default {
       editAlbum: null,
       albums: [],
       currentAlbum: null,
+      nameSearch: null,
+      yearSearch: null,
+      durationSearch: null,
     }
   },
   components:{
@@ -58,6 +86,25 @@ export default {
   computed:{
     user(){
       return this.$store.state.user;
+    },
+    filteredAlbums(){
+       var res = this.albums;
+      if(!this.nameSearch && !this.durationSearch && !this.yearSearch){
+        return this.albums;
+      }
+
+      if(this.nameSearch){
+         res = res.filter(x => x.name.toLowerCase()
+      .includes(this.nameSearch.toLowerCase()))
+      }
+      if (this.durationSearch){
+          res = res.filter(x => x.duration == this.durationSearch)
+      }
+      if(this.yearSearch){
+        res = res.filter(x => x.yeat == this.yearSearch)
+      }
+
+      return res;
     },
   },
   created(){
--- src/components/Playlists/Playlists.vue
+++ src/components/Playlists/Playlists.vue
@@ -6,6 +6,17 @@
       <div v-if="!playlists.length">
       No playlists :( please add from <a @click="showModal(null)" style="color: #42b983; cursor: pointer;">here</a>
     </div>
+     <article style="margin-top: 50px; margin-left: 174px;">
+      <div style="margin: 0" class="wrap">
+        <div style="left: 26.5em; width: 167px;" class="search">
+            <input type="text" v-model="nameSearch" class="searchTerm"
+            placeholder="Name" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+    </div>
+   </article>
       <div v-if="playlists.length" style="display: flex; width: 500px; margin: 0 auto; background: #CCC;
     padding: 20px 0px; color: white ; margin-top: 20px;">
       <div class="w-150">Name</div>
@@ -13,7 +24,7 @@
       <div class="w-115">Tracks</div>
     </div>
      <div style=" width: 500px;" class="tracks-wrapper">
-      <div v-for="(playlist, index) in playlists"
+      <div v-for="(playlist, index) in filteredPlaylists"
       @click="$router.push({name:'playlist', query: { id: playlist.id } })" 
       style="cursor: pointer" :style="{'background':
       index % 2 == 0 ? '#dbdbdb' : '#f0eded'}" class="single-track-row"
@@ -48,6 +59,7 @@ export default {
             isModalVisible: false,
             deleteItem: null,
             isDeleteModalVisible: false,
+            nameSearch: null,
         }
     },
     components:{
@@ -58,6 +70,17 @@ export default {
     user(){
       return this.$store.state.user;
     },
+    filteredPlaylists(){
+      var res = this.playlists;
+      if(!this.nameSearch){
+        return this.playlists;
+      }
+        if(this.nameSearch){
+         res = res.filter(x => x.name.toLowerCase()
+      .includes(this.nameSearch.toLowerCase()))
+      }
+      return res;
+    }
   },
   created(){
       this.getAllPublicPlaylists();
