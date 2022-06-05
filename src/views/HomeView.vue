559d663cd807050fb6d60f0bed14907f9fe5f57b
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-05 22:07:19 +0300

    add searches for tracks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,58 @@
     <div v-if="!tracks.length">
       No Tracks :( please add from <a @click="editTrack(null)" style="color: #42b983; cursor: pointer;">here</a>
     </div>
-    <div v-if="tracks.length" style="display: flex; width: 800px; margin: 0 auto; background: #CCC;
+
+    <article>
+      <div class="wrap">
+        <div style="left: 37em;" class="search">
+            <input type="text" v-model="nameSearch" class="searchTerm"
+            placeholder="Name" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="number" v-model.number="durationSerach" class="searchTerm"
+            placeholder="Duration" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="text" v-model="performerSearch" class="searchTerm"
+            placeholder="Performer" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="text" v-model="arrangerSearch" class="searchTerm"
+            placeholder="Arranger" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+        <div style="left: 37em;" class="search">
+            <input type="text" v-model="typeSearch" class="searchTerm"
+            placeholder="Type" autocomplete="off">
+            <button type="submit" class="searchButton">
+              <img style="width: 20px;" src="../assets/322497_search_icon.png" alt="">
+          </button>
+        </div>
+    </div>
+   </article>
+
+    <div v-if="filteredTracks.length" style="display: flex; width: 800px; margin: 0 auto; background: #CCC;
     padding: 20px 0px; color: white">
-      <div class="w-115">Name</div>
-      <div class="w-115">Duration</div>
-      <div class="w-115">Perfromer</div>
-      <div class="w-115">Writer</div>
-      <div class="w-115">Arranged by</div>
-      <div class="w-115">Type</div>
+      <div class="w-150">Name</div>
+      <div class="w-150">Duration</div>
+      <div class="w-150">Perfromer</div>
+      <div class="w-150">Writer</div>
+      <div class="w-150">Arranged by</div>
+      <div class="w-150">Type</div>
     </div>
     <div class="tracks-wrapper">
-      <div v-for="(track, index) in tracks" :style="{'background': index % 2 == 0 ? '#dbdbdb' : '#f0eded'}" class="single-track-row" :key="index">
+      <div v-for="(track, index) in filteredTracks" :style="{'background': index % 2 == 0 ? '#dbdbdb' : '#f0eded'}" class="single-track-row" :key="index">
         <b class="w-150">{{track.name}}</b>
         <div class="w-150">{{track.duration}}</div>
         <div class="w-150">{{track.performedBy}}</div>
@@ -61,6 +102,11 @@ export default {
         deleteItem: null,
         isDeleteModalVisible: false,
         tracks: [],
+        nameSearch: null,
+        durationSerach: null,
+        performerSearch: null,
+        arrangerSearch: null,
+        typeSearch: null,
       };
     },
    components: {
@@ -73,6 +119,35 @@ export default {
     user(){
       return this.$store.state.user;
     },
+    filteredTracks(){
+      var res = this.tracks;
+      if(!this.nameSearch && !this.durationSerach && !this.performerSearch && !this.arrangerSearch && !this.typeSearch){
+        return this.tracks;
+      }
+
+      if(this.nameSearch){
+         res = res.filter(x => x.name.toLowerCase()
+      .includes(this.nameSearch.toLowerCase()))
+      }
+      if (this.durationSerach){
+          res = res.filter(x => x.duration == this.durationSerach)
+      }
+      if(this.performerSearch){
+         res = res.filter(x => x.performedBy?.toLowerCase()
+      .includes(this.performerSearch.toLowerCase()))
+      }
+      if(this.arrangerSearch){
+        res = res.filter(x => x.arrengedBy?.toLowerCase()
+    .includes(this.arrangerSearch.toLowerCase()))
+      }
+      if(this.typeSearch){
+        res = res.filter(x => x.trackType?.toLowerCase()
+    .includes(this.typeSearch.toLowerCase()))
+      }
+
+      return res;
+      
+    }
   },
   created(){
     this.getAllTracks();
@@ -117,7 +192,57 @@ export default {
     },
 }
 </script>
-<style >
+<style>
+
+body{
+  background: #f2f2f2;
+  font-family: 'Open Sans', sans-serif;
+}
+
+.search {
+  width: 45%;
+  position: relative;
+  display: flex;
+  margin-right: 10px;
+}
+
+.searchTerm {
+  width: 100%;
+  border: 3px solid #42b983 !important;
+  border-right: none;
+  padding: 5px !important;
+  height: 20px;
+  border-radius: 5px 0 0 5px;
+  outline: none;
+  color: #9DBFAF;
+  margin: unset !important;
+}
+
+.searchTerm:focus{
+  color: #00B4CC;
+}
+
+.searchButton {
+  width: 40px;
+  height: 36px;
+  border: 1px solid #42b983;
+  background: #42b983;
+  text-align: center;
+  color: #fff;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  font-size: 20px;
+}
+
+/*Resize the wrap to see the search bar change!*/
+.wrap{
+  width: max-content;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  margin: 0 auto;
+}
 .tracks-wrapper{
   max-width: 800px;
   margin: 0 auto;
